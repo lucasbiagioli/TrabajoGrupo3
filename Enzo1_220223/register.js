183411c1edadceb0254f2315bcd9c60bf4fd72a9
Delgado Enzo Rodrigo <delgadoenzo171@gmail.com>
2023-03-05 23:17:00 -0300

    creacion pag admin_page

---
+++ Enzo1_220223/register.js
@@ -0,0 +1,10 @@
+let registerFormDOM = document.getElementById('registerForm')
+let userNameDOM = document.getElementById ('userName')
+let userEmailDOM = document.getElementById ('userEmail')
+let userPasswordDOM = document.getElementById ('userPassword')
+let confirmPassworDOM = document.getElementById('confirmPasswor')
+let btnRegister = document.getElementById('btnRegister')
+
+registerFormDOM.onsubmit = (e) =>{
+    
+}
